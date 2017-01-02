6f0175d8f2b836b4aa484de070522cba1e7a7521
jamesstorr <james.storr@googlemail.com>
2017-01-02 19:05:10 +0000

    changed to match my git repo

--- saltstack/vagrant_salt_bootstrap/bootstrap.sh
+++ saltstack/vagrant_salt_bootstrap/bootstrap.sh
@@ -8,7 +8,8 @@ apt-get --yes -q install python-git salt-master salt-minion
 cp /srv/vagrant_salt_bootstrap/master /etc/salt/master
 cp /srv/vagrant_salt_bootstrap/minion /etc/salt/minion
 # add the https://github.com/buddycloud/saltstack repo (the heavy lifting part)
-git clone https://github.com/buddycloud/saltstack.git /srv/buddycloud_saltstack_repo
+#git clone https://github.com/buddycloud/saltstack.git /srv/buddycloud_saltstack_repo
+git clone https://github.com/tsaserv/saltstack.git /srv/buddycloud_saltstack_repo
 mkdir /srv/my_saltstack_repo
 restart salt-minion
 restart salt-master
